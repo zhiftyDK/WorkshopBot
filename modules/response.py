711c07668c5143e7362c867090d1cff0ec420af5
CodeZhifty <juulstausholmoscar@gmail.com>
2023-08-01 00:48:03 +0200

    Added temp response to bot mention

--- modules/response.py
+++ modules/response.py
@@ -1,3 +1,3 @@
 async def handle_response(message, client):
     content = str(message.content).lower().replace(f"<@{client.user.id}>", "")
-    # Implement carter here :)
+    await message.channel.send(f"Sorry but im currently not able to start a conversation!", reference=message)
