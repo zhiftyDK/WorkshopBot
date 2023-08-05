1ad811530651281ca2ae67e8a8ba62052b36bee7
CodeZhifty <juulstausholmoscar@gmail.com>
2023-08-05 11:53:47 +0200

    Removed textgeneration again

--- modules/textgeneration.py
+++
@@ -1,6 +0,0 @@
-from gpt4all import GPT4All
-model = GPT4All(model_name="ggml-model-gpt4all-falcon-q4_0.bin", model_path="./models/", allow_download=False, n_threads=4)
-def generate(prompt, system_prompt):
-    with model.chat_session(system_prompt=system_prompt):
-        output = model.generate(prompt, max_tokens=4096, n_batch=128, temp=0)
-        return output
